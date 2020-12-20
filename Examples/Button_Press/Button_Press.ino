b6804119a5ab5a17d375a54fe914c0ccfa2d8101
TrinhNgocTuan <tuan.trinhngoc95@gmail.com>
2020-12-20 13:32:29 +0700

    first commit

--- Examples/Button_Press/Button_Press.ino
+++ Examples/Button_Press/Button_Press.ino
@@ -1,18 +1,31 @@
 #include "Button.h"
-Button button(2);
+#define led  A0 
+bool staLed=0;
+const int BUTTON_PIN = 2;     
+  
+Button button(BUTTON_PIN);
 void setup() {
   Serial.begin(9600);
+  pinMode(led,OUTPUT);
 }
-
 void loop() {
+  //phải gọi hàm này trước isPressed() và isReleased()
   button.handle();
+  //kiểm tra click phím
+  if(button.isClicked())
+  {
+    staLed=!staLed;
+    digitalWrite(led,staLed);
+    Serial.println("click");
+  }
+  //kiểm tra nhấn giữ
   if(button.isPressed())
   {
-    Serial.println("press ok");
+    Serial.println("press");
   }
+  //kiểm tra nhả phím
   if(button.isReleased())
   {
     Serial.println("release");
   }
-  delay(100);
 }
